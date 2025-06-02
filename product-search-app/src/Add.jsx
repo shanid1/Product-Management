e7b6f9630ac7eb4bcbf2dd8e65958252c31df82e
shanid1 <shanidktk18@gmail.com>
2025-06-03 00:27:05 +0530

    Fix: Minor bug fix and small updates made

--- product-search-app/src/Add.jsx
+++ product-search-app/src/Add.jsx
@@ -45,7 +45,7 @@ function Add(){
     <form>
     <input placeholder="Name" value={names}  onChange={(e)=>{setName(e.target.value)}}></input>
     <input placeholder="Price" value={prices} onChange={(e)=>{setPrice(e.target.value)}}></input>
-    <input placeholder="Branch)" value={branchs} onChange={(e)=>{setBranch(e.target.value)}}></input>
+    <input placeholder="Branch:" value={branchs} onChange={(e)=>{setBranch(e.target.value)}}></input>
     <input placeholder="Stock" value={stocks} onChange={(e)=>{setStock(e.target.value)}}></input>
     <input placeholder="Sales" value={saless} onChange={(e)=>{setSales(e.target.value)}}></input>
     <button type="button" onClick={addProduct}>Add Product</button>
